33ac6678bef75cf5154c2805cd01259d4f2bb647
ftm2-cin <115846360+SENAC-Felipe@users.noreply.github.com>
2023-12-06 01:34:51 -0300

    hotfix: goBack function

--- src/app/view-message/view-message.page.ts
+++ src/app/view-message/view-message.page.ts
@@ -1,7 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonicModule, Platform } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { DataService, Message } from '../services/data.service';
 
 @Component({
@@ -13,17 +12,15 @@ export class ViewMessagePage implements OnInit {
   public message!: Message;
   private data = inject(DataService);
   private activatedRoute = inject(ActivatedRoute);
-  private platform = inject(Platform);
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
   ngOnInit() {
     const id = this.activatedRoute.snapshot.paramMap.get('id') as string;
     this.message = this.data.getMessageById(parseInt(id, 10));
   }
 
-  getBackButtonText() {
-    const isIos = this.platform.is('ios')
-    return isIos ? 'Inbox' : '';
+  goBack() {
+    this.navCtrl.back();
   }
 }
