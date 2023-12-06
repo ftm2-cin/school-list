5d181b1039e3433ef9f20ef7752e1e129255bc01
ftm2-cin <ftm2@cin.ufppe.br>
2023-12-06 19:55:19 -0300

    feat: location && contact pages

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { DataService, School } from '../services/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -7,14 +9,38 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
+  public schoolContact: School | undefined;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    private dataService: DataService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.dataService.getSchoolBycoEntidade(parseInt(id, 10)).subscribe(
+      (school) => {
+        this.schoolContact = school; // Assign school location data to schoolContact
+      },
+      (error) => {
+        console.error('Error fetching school:', error);
+        // Handle the error as needed
+      }
+    );
   }
 
   goBack() {
     this.navCtrl.pop();
   }
 
+  redirectToWhatsApp() {
+    if (this.schoolContact && this.schoolContact.nuTelefone) {
+      const whatsappLink = `https://api.whatsapp.com/send?phone=${this.schoolContact.nuTelefone}`;
+      window.open(whatsappLink, '_system', 'location=yes');
+    } else {
+      console.error('Phone number not available');
+      // Handle the case where the phone number is not available
+    }
+  }
 }
--- src/app/location/location.page.ts
+++ src/app/location/location.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { DataService, School } from '../services/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-location',
@@ -7,10 +9,25 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./location.page.scss'],
 })
 export class LocationPage implements OnInit {
+  public schoolLocation: School | undefined;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    private dataService: DataService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.dataService.getSchoolBycoEntidade(parseInt(id, 10)).subscribe(
+      (school) => {
+        this.schoolLocation = school; // Assign school location data to schoolLocation
+      },
+      (error) => {
+        console.error('Error fetching school:', error);
+        // Handle the error as needed
+      }
+    );
   }
 
   async goBack() {
