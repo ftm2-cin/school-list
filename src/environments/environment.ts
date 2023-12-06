ef33f7875be5f2e9daaae39d6da4bdb7ac43eb50
ftm2-cin <ftm2@cin.ufppe.br>
2023-12-06 20:45:50 -0300

    setup: maps api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapsKey: 'AIzaSyAkkaInGN_qyU2bT9pd3y_HIWFbU51vs6o'
 };
 
 /*
